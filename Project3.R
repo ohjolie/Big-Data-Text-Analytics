9451d5de37c4c1dc0c7eb1b32796edfbd10ed177
yzlhbg <1281161875@qq.com>
2019-04-29 16:33:00 -0400

    Update Project3.R
    Added Question C

--- Project3.R
+++ Project3.R
@@ -3,6 +3,87 @@ library(quanteda)
 library(readtext)
 library(tokenizers)
 
+# Question C
+ws<-VCorpus(DirSource(".", ignore.case = TRUE, mode = "text"))
+ws
+
+inspect(ws)
+str(ws)
+test1<-ws[[1]]
+##
+pg1<-ws[[13]]
+pg2<-ws[[9]]
+pg3<-ws[[8]]
+pg4<-ws[[1]]
+pg5<-ws[[10]]
+pg6<-ws[[6]]
+pg7<-ws[[5]]
+pg8<-ws[[4]]
+pg9<-ws[[12]]
+pg10<-ws[[2]]
+pg11<-ws[[3]]
+##
+wsdtm<-DocumentTermMatrix(ws)
+wsdtm
+wstdm<-TermDocumentMatrix(ws)
+wstdm
+inspect(wstdm[1:12, 1])
+
+test1tf<-termFreq(test1)
+test1tf
+test1df<-as.data.frame(test1tf)
+test1df
+
+wslow<-tm_map(ws, content_transformer(tolower))
+wslow
+
+removeNumPunct<-function(x) gsub("[^[:alpha:][:space:]]*", "", x)
+wscl<-tm_map(wslow, content_transformer(removeNumPunct))
+
+myStopWords<-c(stopwords('english'))
+myStopWords
+
+wsstop<-tm_map(wscl, removeWords, myStopWords)
+inspect(wsstop[1:13])
+#following change needed
+wstdm2<-TermDocumentMatrix(wsstop[1], control = list(wordlengths = c(1, Inf)))
+wstdm2
+
+freqTerms<-findFreqTerms(wstdm2, lowfreq = 4)
+freqTerms
+
+statesAssoc<-findAssocs(wstdm2, "states", 0.5)
+statesAssoc
+
+termFreq<-rowSums(as.matrix(wstdm2))
+termFreqSub<-subset(termFreq, termFreq >= 6)
+termFreqdf<-as.data.frame(names(termFreq), freq = termFreq)
+termFreq
+
+wstdm2
+
+sparsetdm2<-removeSparseTerms(wstdm2, sparse = 0.75)
+inspect(sparsetdm2)
+sparsetdm2
+
+#p3stop<-wsstop[[1]]
+#inspect(p3stop)
+
+termFreqSub2<-subset(termFreq, termFreq >= 40)
+fit <- hclust(dist(termFreqSub,method = "euclidean"), method = "ward.D2")
+plot(fit)
+
+word.freq<-sort(termFreq, decreasing = T)
+word.freq
+
+#install.packages("wordcloud")
+library("wordcloud")
+
+pal<-brewer.pal(9, "BuGn")
+pal<-pal[-(1:4)]
+wordcloud(words = names(word.freq), freq = word.freq, min.freq =  3, random.order = F, colors = pal)
+#end of Question C
+
 
 data <- readtext('DrJekyllAndMrHyde.txt')
 
