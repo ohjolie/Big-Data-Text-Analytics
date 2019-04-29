ef2ac4ecc3100f7b4e452829fbe58f08a07acedf
yzlhbg <1281161875@qq.com>
2019-04-29 16:35:39 -0400

    Update Project3.R
    Added Q C

--- Project3.R
+++ Project3.R
@@ -3,6 +3,47 @@ library(quanteda)
 library(readtext)
 library(tokenizers)
 
+#Question B
+data <- readtext('DrJekyllAndMrHyde.txt')
+
+#################b###############
+#delete \n
+test <- gsub("\r?\n|\r", ' ',data$text)
+
+#split into sentences using tokens
+sentence1 <- tokens(test, what = "sentence", remove_url=TRUE)
+sentence1
+sentence_words.1 <- tokenize_words(sentence1[[1]])
+lengths.1 <- sapply(sentence_words.1, length)
+longest.1 <- sort(lengths.1, TRUE)
+longest.1
+for (n in 1:10) {
+  for (i in 1:1345){
+    if(lengths.1[i] == longest.1[n]){
+      print(paste0("The ", n ,"th longest sentence is:"))
+      print(sentence1[[1]][i])
+      print('')} 
+  } 
+}
+
+
+
+#split into sentences using tokenize_sentences
+sentence2 <- tokenize_sentences(test)
+
+sentence_words.2 <- tokenize_words(sentence2[[1]])
+lengths.2 <- sapply(sentence_words.2, length)
+longest.2 <- sort(lengths.2, TRUE)
+longest.2
+for (n in 1:10) {
+  for (i in 1:1513){
+    if(lengths.2[i] == longest.2[n]){
+      print(paste0("The ", n ,"th longest sentence is:"))
+      print(sentence2[[1]][i])} 
+  } 
+}
+#end of Question B
+
 # Question C
 ws<-VCorpus(DirSource(".", ignore.case = TRUE, mode = "text"))
 ws
@@ -45,7 +86,7 @@ myStopWords
 
 wsstop<-tm_map(wscl, removeWords, myStopWords)
 inspect(wsstop[1:13])
-#following change needed
+
 wstdm2<-TermDocumentMatrix(wsstop[1], control = list(wordlengths = c(1, Inf)))
 wstdm2
 
@@ -83,44 +124,3 @@ pal<-brewer.pal(9, "BuGn")
 pal<-pal[-(1:4)]
 wordcloud(words = names(word.freq), freq = word.freq, min.freq =  3, random.order = F, colors = pal)
 #end of Question C
-
-
-data <- readtext('DrJekyllAndMrHyde.txt')
-
-#################b###############
-#delete \n
-test <- gsub("\r?\n|\r", ' ',data$text)
-
-#split into sentences using tokens
-sentence1 <- tokens(test, what = "sentence", remove_url=TRUE)
-sentence1
-sentence_words.1 <- tokenize_words(sentence1[[1]])
-lengths.1 <- sapply(sentence_words.1, length)
-longest.1 <- sort(lengths.1, TRUE)
-longest.1
-for (n in 1:10) {
-  for (i in 1:1345){
-    if(lengths.1[i] == longest.1[n]){
-      print(paste0("The ", n ,"th longest sentence is:"))
-      print(sentence1[[1]][i])
-      print('')} 
-  } 
-}
-
-
-
-#split into sentences using tokenize_sentences
-sentence2 <- tokenize_sentences(test)
-
-sentence_words.2 <- tokenize_words(sentence2[[1]])
-lengths.2 <- sapply(sentence_words.2, length)
-longest.2 <- sort(lengths.2, TRUE)
-longest.2
-for (n in 1:10) {
-  for (i in 1:1513){
-    if(lengths.2[i] == longest.2[n]){
-      print(paste0("The ", n ,"th longest sentence is:"))
-      print(sentence2[[1]][i])} 
-  } 
-}
-
